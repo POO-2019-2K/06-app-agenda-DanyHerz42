1333b741504083d25f0ee87dfe361946137793e6
DanyHerz42 <dhernandez30@ucol.mx>
2019-05-21 18:38:24 -0500

    prueba

--- js/contact.js
+++ js/contact.js
@@ -1,3 +1,26 @@
-export default class Contact{
-    
+export default class Contacto{
+    constructor(contacto){
+        this._name = contacto.name;
+        this._phone = contacto.phone;
+        this._birthdate = contacto.birthdate;
+        this._email = contacto.email;
+        this._obtenerFechaString(this._birthdate);
+        this._obtenerObjectFecha(this._birthdate);
+    }
+
+    _obtenerFechaString(fecha){
+        let s = fecha.split("-");
+        let stringFecha = s[2] + "/" + s[1] + "/" + s[0];
+        console.log(stringFecha);
+    }
+
+    _obtenerObjectFecha(fecha){
+        let s = fecha.split("-");
+        let oFecha = new Date(s[0],s[1] - 1,s[2]);
+        console.log(oFecha);
+    }
+
+    _guardarEnLocalStorage(){
+        
+    }
 }
--- js/main.js
+++ js/main.js
@@ -1,14 +1,13 @@
+import Contacto from "./contact.js";
 class Main{
     constructor(){
         document.querySelector("#btnAdd").addEventListener("click", () => {
-        this._validarFormulario();
-          
-        })
+            this._validarFormulario();    
+        });
     }
 
     _validarFormulario(){
         let form = document.querySelector("#form");
-        
         if(form.checkValidity() === true){
             let contact = {
                 name: document.querySelector("#name").value,
@@ -16,7 +15,7 @@ class Main{
                 birthdate: document.querySelector("#birthdate").value,
                 email: document.querySelector("#email")
             };   
-            console.log(contact);
+            let contacto = new Contacto(contact);
         }
         form.classList.add("was-validated");
 
